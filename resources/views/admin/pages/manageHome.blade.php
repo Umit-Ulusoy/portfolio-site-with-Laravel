4924415cbb1e86f556d0822888278773a0ad6442
Umit-Ulusoy <88715653+Umit-Ulusoy@users.noreply.github.com>
2022-10-16 02:27:09 +0300

    ManageHome view file is done

---
+++ resources/views/admin/pages/manageHome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.default')
+@section('title', 'Manage Home')
+@section('content')
+
+<h1>Manage Home Page</h1>
+You can edit data of the home page <br />
+<form action="/admin/home" method="POST">
+    @csrf
+    @method('put')
+    <input type="text" name="Headline" placeholder="Enter the headline" value="{{ $home->Headline }}" /> <br />
+    <textarea name="Subline" >{{ $home->Subline }}</textarea> <br />
+    <button type="submit" >Save Changes</button>
+
+    @stop
