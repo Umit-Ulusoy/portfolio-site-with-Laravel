427d0ee18cd1f8cc2d383034d2087e25aca8d563
Umit-Ulusoy <88715653+Umit-Ulusoy@users.noreply.github.com>
2022-10-16 02:46:49 +0300

    ManageContact view file is done

---
+++ resources/views/admin/pages/ManageContact.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+@section('title', 'Manage Contact')
+@section('content')
+
+<h1>Manage Contact Page</h1>
+You can edit the data of the contact page <br />
+<form action="/admin/contact" method="POST">
+    @csrf
+    @method('put')
+    <textarea name="Content">{{ $contact->Content }}</textarea> <br />
+    <input type="text" name="Adress" value="{{ $contact->Adress }}" placeholder="Enter a adress" /> <br />
+    <input type="text" name="PhoneNumber" value="{{ $contact->PhoneNumber }}" placeholder="Enter a phone number" /> <br />
+    <button type="submit" >Save Changes</button>
+</form>
+
+@stop
