a1db769d0d888e1135b1376d344ce2815b29b265
Umit-Ulusoy <88715653+Umit-Ulusoy@users.noreply.github.com>
2022-10-16 03:15:38 +0300

    made a lot changes in ManageSkills view file

--- resources/views/admin/pages/ManageSkills.blade.php
+++ resources/views/admin/pages/ManageSkills.blade.php
@@ -3,13 +3,16 @@
 @section('content')
 <h1>Manage Skills Page</h1>
 You can edit the data of the skills page <br />
+@foreach($skills as $skill)
 <form>
     @csrf
     @method('put')
+    <input type="hidden" name="id" value="{{ $skill->Id }}" />
     <input type="text" name="Skill" value="{{ $skill->Skill }}" placeholder="Enter the skill" /> <br />
     <textarea name="Description" placeholder="Enter the description of the skill" >{{ $skill->Description }}</textarea> <br />
     <button type="submit" name="action" >Save</button>
     <button type="submit" name="action" >Delete</button>
 </form>
+@endforeach
 
 @stop
